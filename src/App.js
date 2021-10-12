54b6313a000a61b599c9f0b04715e0320af597ed
petumazo <davidalvarezmazzo@gmail.com>
2021-10-12 11:46:55 -0500

    Add simple style to navbar.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import Nav from './components/Nav';
+import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Profile from './pages/Profile';
 
 function App() {
   return (
     <Router className="App">
-      <Nav />
-      <h1>Space Travelers</h1>
+      <Header />
       <Route exact path="/" component={Rockets} />
       <Route exact path="/Profile" component={Profile} />
     </Router>
